9f7e7d9e2fdcb3141f338da4b460ae62d3adc6cf
courtneyehancock <courtneyehanco1k@TSS-OPWD-001A.local>
2024-06-18 10:30:54 -0400

    Edit header

--- footer.php
+++ footer.php
@@ -22,13 +22,11 @@
 
   <div class="container-fluid final-footer">
     <div class="row d-flex align-items-center">
-      <div class="col-lg-6">
+      <div class="col-lg-8">
         <p> © <?php echo date("Y"); ?> University of Florida Conference Department | <a href="https://pwd.aa.ufl.edu/website-feedback/">Website Feedback</a> | <a href="https://privacy.ufl.edu/privacy-policies-and-procedures/onlineinternet-privacy-statement/">UF Privacy Policy</a></p>
       </div>
       <div class="col-lg-2">
-      </div>
-      <div class="col-lg-2">
-        <a href="https://conferences.aa.ufl.edu/" target="_blank"><img alt="PWD Logo" src="https://pwd.aa.ufl.edu/wp-content/uploads/2020/12/conferences-white.png"/></a>
+        <a href="https://conferences.aa.ufl.edu/" target="_blank"><img alt="Conference Logo" src="https://pwd.aa.ufl.edu/wp-content/uploads/2020/12/conferences-white.png"/></a>
       </div>
       <div class="col-lg-2">
         <a href="https://www.ufl.edu/" target="_blank"><img alt="UF Logo" src="https://pwd.aa.ufl.edu/wp-content/uploads/2021/08/UF-SVG.svg"/></a>
--- header.php
+++ header.php
@@ -4,19 +4,33 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php bloginfo('name'); ?></title>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-PZVDB2M');</script>
+  <!-- End Google Tag Manager -->
+  <!--Facebook Meta Tag-->
+  <meta name="facebook-domain-verification" content="rtkjhpxmfz6ny9m5emxv1v6j2up7jk" />
+
   <!--Classica Web Font-->
   <link rel="stylesheet" href="https://use.typekit.net/roz5znz.css">
   <!--font awesome-->
   <script src="https://kit.fontawesome.com/45fdff44f1.js" crossorigin="anonymous"></script>
+
   <?php wp_head(); ?>
 </head>
 
 <body <?php body_class();?>>
-
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PZVDB2M"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
 <header>
   <div class="align-content-center">
     <div class="row main-nav">
-      <div class="col-md-2" id="site-logo">
+      <div class="col-md-3" id="site-logo">
           <div>
           <!--If/else for Logo and Site Title-->
           <?php if(get_header_image() == '') { ?>
@@ -30,7 +44,13 @@
         </div>
       </div>
 
-      <div class="col-md-10" id="site-nav">
+      <div class="col-md-9 d-flex align-items-center justify-content-end search-nav">
+        <div class="col-md-4 text-right">
+          <!--Middle Right Footer-->
+          <?php dynamic_sidebar('head-social-widget'); ?>
+        </div>
+    </div>
+      <div class="col-md-12" id="site-nav">
         <!--Navigation-->
         <?php wp_nav_menu(array(
           'theme_location' => 'header-menu',
