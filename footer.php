0785d73985512269067fc07def8fb117ab9baf20
courtneyehancock <courtneyehanco1k@TSS-OPWD-001A.local>
2024-06-18 10:52:23 -0400

    Edit logo sizing in footer

--- footer.php
+++ footer.php
@@ -22,10 +22,10 @@
 
   <div class="container-fluid final-footer">
     <div class="row d-flex align-items-center">
-      <div class="col-lg-8">
+      <div class="col-lg-7">
         <p> © <?php echo date("Y"); ?> University of Florida Conference Department | <a href="https://pwd.aa.ufl.edu/website-feedback/">Website Feedback</a> | <a href="https://privacy.ufl.edu/privacy-policies-and-procedures/onlineinternet-privacy-statement/">UF Privacy Policy</a></p>
       </div>
-      <div class="col-lg-2">
+      <div class="col-lg-3">
         <a href="https://conferences.aa.ufl.edu/" target="_blank"><img alt="Conference Logo" src="https://pwd.aa.ufl.edu/wp-content/uploads/2020/12/conferences-white.png"/></a>
       </div>
       <div class="col-lg-2">
